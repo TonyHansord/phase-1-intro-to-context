cea0d8829dbd2d26951e01aa0c924843d98e9b45
TonyHansord <59306359+TonyHansord@users.noreply.github.com>
2022-12-15 10:01:43 +1100

    created first function

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+function createEmployeeRecord(employee){
+
+return {
+firstName: employee[0],
+familyName: employee[1],
+title: employee[2],
+payPerHour: employee[3],
+timeInEvents: [],
+timeOutEvents: []
+
+};
+}
